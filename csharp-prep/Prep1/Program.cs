8d7b6729838dce2734625c40f9a642e9ade9ee1c
araltos <leonardo.jara.saltos@gmail.com>
2024-03-08 18:29:02 -0700

    Prep 1 done

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+       
+        Console.Write("What is your first name? ");
+        string name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+        // print the results
+
+        Console.WriteLine($"Your name is {last_name}, {last_name} {name}.");
     }
 }
