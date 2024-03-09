aaa016e82166a5f3a8002b1d8e6c52d7c0fd5a50
araltos <leonardo.jara.saltos@gmail.com>
2024-03-09 08:17:21 -0700

    Prep 2 done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("What is your grade percentage? ");
+        string in_grade  = Console.ReadLine();
+        int grade = int.Parse(in_grade);
+
+        if (grade >= 90)
+         {
+            Console.WriteLine("You have an A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("You have a B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("You have a C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("You have a D ");
+        }
+        else if (grade < 60)
+        {
+            Console.WriteLine("You have a F");
+        }
+        else{
+            Console.WriteLine("You must enter a percentage of your grade");
+        }
+
     }
 }
