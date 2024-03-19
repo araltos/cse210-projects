09dfde314a2637d5311bdef2c9dde50b6f375cee
araltos <leonardo.jara.saltos@gmail.com>
2024-03-18 21:10:39 -0600

    Prep 3 done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        int numrespond;
+        do 
+        {
+            Console.Write("What is the magic number? ");
+            string response = Console.ReadLine();
+
+
+          if (!int.TryParse(response, out numrespond))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+                continue;
+            }
+
+            if (numrespond < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (numrespond > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it! The magic number was: " + number);
+            }
+
+        } while (numrespond != number);
     }
 }
