34f98c01c3f2e3f952f1ad560e2e688b45016c0a
araltos <leonardo.jara.saltos@gmail.com>
2024-04-05 13:21:25 -0600

    Done Foundation 1 and 2

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,95 @@
 using System;
+using System.Collections.Generic;
+
+public abstract class MediaItem
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+}
+
+
+public abstract class MediaPlayer
+{
+    public abstract void Play();
+    public abstract void Pause();
+    public abstract void Stop();
+}
+public class YouTubeVideo : MediaItem
+{
+    public int LengthSeconds { get; set; }
+    private List<Comment> comments = new List<Comment>();
+
+    public int NumberOfComments()
+    {
+        return comments.Count;
+    }
+
+    public void AddComment(Comment comment)
+    {
+        comments.Add(comment);
+    }
+
+    public void DisplayComments()
+    {
+        foreach (var comment in comments)
+        {
+            Console.WriteLine($"Comment by {comment.Commenter}: {comment.Text}");
+        }
+    }
+}
+
+
+public class Comment
+{
+    public string Commenter { get; set; }
+    public string Text { get; set; }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+
+        var videos = new List<YouTubeVideo>
+        {
+            new YouTubeVideo
+            {
+                Title = "Video 1",
+                Author = "Author 1",
+                LengthSeconds = 120
+            },
+            new YouTubeVideo
+            {
+                Title = "Video 2",
+                Author = "Author 2",
+                LengthSeconds = 180
+            },
+            new YouTubeVideo
+            {
+                Title = "Video 3",
+                Author = "Author 3",
+                LengthSeconds = 150
+            }
+        };
+
+   
+        videos[0].AddComment(new Comment { Commenter = "User 1", Text = "Great video!" });
+        videos[0].AddComment(new Comment { Commenter = "User 2", Text = "Awesome content!" });
+
+        videos[1].AddComment(new Comment { Commenter = "User 3", Text = "Interesting topic." });
+
+        videos[2].AddComment(new Comment { Commenter = "User 4", Text = "Nice work!" });
+        videos[2].AddComment(new Comment { Commenter = "User 5", Text = "Keep it up." });
+
+    
+        foreach (var video in videos)
+        {
+            Console.WriteLine($"Title: {video.Title}");
+            Console.WriteLine($"Author: {video.Author}");
+            Console.WriteLine($"Length (seconds): {video.LengthSeconds}");
+            Console.WriteLine($"Number of Comments: {video.NumberOfComments()}");
+            video.DisplayComments();
+            Console.WriteLine();
+        }
     }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,9 +1,174 @@
 using System;
+using System.Collections.Generic;
+
+public class Address
+{
+    private string streetAddress;
+    private string city;
+    private string state;
+    private string country;
+
+    public Address(string street, string city, string state, string country)
+    {
+        this.streetAddress = street;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+    }
+
+    public bool IsInUSA()
+    {
+        return country.Equals("USA", StringComparison.OrdinalIgnoreCase);
+    }
+
+    public string GetFullAddress()
+    {
+        return $"{streetAddress}\n{city}, {state}\n{country}";
+    }
+}
+
+
+public class Customer
+{
+    private string name;
+    private Address address;
+
+    public Customer(string name, Address address)
+    {
+        this.name = name;
+        this.address = address;
+    }
+
+    public bool IsInUSA()
+    {
+        return address.IsInUSA();
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public Address GetAddress()
+    {
+        return address;
+    }
+}
+
+
+public class Product
+{
+    private string name;
+    private string productId;
+    private decimal pricePerUnit;
+    private int quantity;
+
+    public Product(string name, string productId, decimal pricePerUnit, int quantity)
+    {
+        this.name = name;
+        this.productId = productId;
+        this.pricePerUnit = pricePerUnit;
+        this.quantity = quantity;
+    }
+
+    public decimal GetTotalCost()
+    {
+        return pricePerUnit * quantity;
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public string GetProductId()
+    {
+        return productId;
+    }
+}
+
+
+public class Order
+{
+    private List<Product> products;
+    private Customer customer;
+
+    public Order(Customer customer)
+    {
+        products = new List<Product>();
+        this.customer = customer;
+    }
+
+    public void AddProduct(Product product)
+    {
+        products.Add(product);
+    }
+
+    public decimal CalculateTotalCost()
+    {
+        decimal totalCost = 0;
+        foreach (var product in products)
+        {
+            totalCost += product.GetTotalCost();
+        }
+
+        
+        totalCost += customer.IsInUSA() ? 5 : 35;
+
+        return totalCost;
+    }
+
+    public string GetPackingLabel()
+    {
+        string label = "Packing Label:\n";
+        foreach (var product in products)
+        {
+            label += $"{product.GetName()} (ID: {product.GetProductId()})\n";
+        }
+        return label;
+    }
+
+    public string GetShippingLabel()
+    {
+        return $"Shipping Label:\n{customer.GetName()}\n{customer.GetAddress().GetFullAddress()}";
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        
+        var address1 = new Address("123 Main St", "Anytown", "CA", "USA");
+        var address2 = new Address("456 Oak Ave", "Another City", "TX", "Mexico");
+
+      
+        var customer1 = new Customer("John Doe", address1);
+        var customer2 = new Customer("Jane Smith", address2);
+
+   
+        var product1 = new Product("Product A", "A123", 10.5m, 3);
+        var product2 = new Product("Product B", "B456", 8.75m, 5);
+        var product3 = new Product("Product C", "C789", 15.25m, 2);
+
+        
+        var order1 = new Order(customer1);
+        order1.AddProduct(product1);
+        order1.AddProduct(product2);
+
+        var order2 = new Order(customer2);
+        order2.AddProduct(product2);
+        order2.AddProduct(product3);
+
+        
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine($"Total Price: ${order1.CalculateTotalCost()}");
+
+        Console.WriteLine();
+
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"Total Price: ${order2.CalculateTotalCost()}");
     }
 }
